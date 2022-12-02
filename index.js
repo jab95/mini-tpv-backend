d8fe55ffbe058f7462491970743b2eb1bfafb58a
jose <josejab95@gmail.com>
2022-12-03 00:23:08 +0100

    prueba socket

--- index.js
+++ index.js
@@ -38,7 +38,14 @@ appDatabase.initDatabase();
     const wss = new WebSocketServer.Server({server})
 
     wss.on("connection",function connection(ws){
-        console.log("cliente conectado")
+
+      //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
     })
         // // const subscriber = client.duplicate();
 
