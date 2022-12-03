efabaaeb139349aa0ff4475cdd33d0c2467e33e1
jose <josejab95@gmail.com>
2022-12-03 01:20:01 +0100

    prueba

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const http = require("https");
 const express = require("express");
 const app = express();
 const appDatabase = require("./public/javascript/appDatabase");
@@ -21,17 +21,17 @@ appDatabase.initDatabase();
     // });
 
 
-    // const options = 
-    // {
-    //     key: fs.readFileSync("./certificados/key-rsa.pem","utf8"),
-    //     cert: fs.readFileSync("./certificados/cert.pem","utf8"),
-    //     requestCert: false,
-    //     rejectUnauthorized: false,
-    //     secureOptions: require("constants").SSL_OP_NO_SSLv3 | require("constants").SSL_OP_NO_TLSv1,
-    //     // honorCipherOrder:true
-    // }
+    const options = 
+    {
+        key: fs.readFileSync("certificados/key-rsa.pem","utf8"),
+        cert: fs.readFileSync("certificados/cert.pem","utf8"),
+        requestCert: false,
+        rejectUnauthorized: false,
+        secureOptions: require("constants").SSL_OP_NO_SSLv3 | require("constants").SSL_OP_NO_TLSv1,
+        // honorCipherOrder:true
+    }
 
-    const server = http.createServer(app).listen(config2.PORT,()=>{
+    const server = http.createServer(options,app).listen(config2.PORT,()=>{
         console.log("listening ",process.env.PORT)
     })
     
